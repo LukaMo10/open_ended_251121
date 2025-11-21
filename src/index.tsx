b23d8268bedb43b5aa0e0cb402afef6360bb475d
LukaMo10 <crisbale001@gmail.com>
2025-11-21 16:16:35 +0800

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // 即使没有css文件也不报错，vite会自动处理
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
